bf3421d4dfbd799d1d60f11a4ac076e14446ed86
GKAtwood <gabrielle.k85@gmail.com>
2020-07-02 11:31:56 -0600

    first commit

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import {Link} from 'react-router-dom';
+
 import logo from "./../../images/dm_white_logo.png";
 import "./Nav.css";
 
@@ -10,8 +12,12 @@ class Nav extends Component {
           <img src={logo} alt="dm logo" />
         </div>
         <ul>
-          <li>Home</li>
-          <li>Topics</li>
+          <li>
+            <Link to ='/'>Home</Link>
+            </li>
+          <li>
+          <Link to ='/topics'>Topics</Link>
+            </li>
         </ul>
       </div>
     );
